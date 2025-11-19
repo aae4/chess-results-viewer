fec50bf62954489b19c32b0f994f9671ac2cbee8
lionsgate <lionsgate@gmail.com>
2025-11-19 19:41:09 +0300

    readme

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-// Замените 'chess-results-viewer' на НАЗВАНИЕ ВАШЕГО РЕПОЗИТОРИЯ на GitHub
 const repoName = 'chess-results-viewer';
 
 export default defineConfig({
-  // Указываем базовый путь для сборки
   base: `/${repoName}/`,
   plugins: [vue()],
   // vueDevTools(),
